524e456cec523856ed92f0b3bdde46a90506a0d5
Jetsadakorn <my1471@hotmail.com>
2023-02-12 14:11:37 +0700

    create form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+import { v4 } from 'uuid';
 
 function App() {
+  const [name,setName] = useState("")
+  const [list,setList] = useState([])
+
+  const submitData=(e)=>{
+    e.preventDefault()
+    const newItem={
+      id:v4(),
+      title:name
+    }
+    setList([...list,newItem])
+    setName('')
+  }
   return (
     <section className='container'>
       <h1>To Do List</h1>
-      <form className='form-group'>
-        <input type="text" className="text-input"></input>
-        <button type='submit' className='submit-button'>เพิ่มข้อมูล</button>
+      <form className='form-group' onSubmit={submitData}>
+        <div className='form-control'>
+          <input type="text" className="text-input"
+          onChange={(e)=>setName(e.target.value)}
+          value={name}
+          />
+          <button type='submit' className='submit-button'>เพิ่มข้อมูล</button>
+        </div>
       </form>
     </section>
     );
