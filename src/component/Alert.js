e8709b3fb460c8efac130fdf66a149b28b200eeb
Jetsadakorn <my1471@hotmail.com>
2023-02-15 14:51:56 +0700

    Timeout

--- src/component/Alert.js
+++ src/component/Alert.js
@@ -1,4 +1,12 @@
-const Alert =({msg,type})=>{
+import { useEffect } from "react"
+
+const Alert =({msg,type,setAlert,list})=>{
+    useEffect(()=>{
+        const timeOut = setTimeout(()=>{
+            setAlert({show:false,msg:'',type:''})
+        },3000)
+        return()=>clearTimeout(timeOut)
+    },[list])
     return(
         <p className={`alert ${type}`}>{msg}</p>
     )
